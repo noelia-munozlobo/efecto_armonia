0058e4e5acbe0aaca74a7c50a2506be703a7e586
FiorellaCG <fiochaves1303@gmail.com>
2025-11-17 10:38:11 -0600

    Cambios

--- recursos/models.py
+++ recursos/models.py
@@ -11,5 +11,6 @@ class Recursos(models.Model):
     descripcion = models.CharField(max_length=200, blank=True, null=False)
     nombre_recurso = models.CharField(max_length=100)
     usuario = models.ForeignKey("usuarios.Usuario", on_delete=models.CASCADE, related_name="recursos")
+    imagen_recurso = models.ImageField(upload_to='recursos/', blank=True, null=True)
     def __str__(self):
         return self.nombre_recurso
--- usuarios/views.py
+++ usuarios/views.py
@@ -12,24 +12,28 @@ class UsuarioCreateView(ListCreateAPIView):
     serializer_class = UsuarioSerializer
 
 class UsuarioLoginView(APIView):
-    def post(self,request):
+    def post(self, request):
         nombre_usuario = request.data.get("username")
         password = request.data.get("password")
 
-
-        usuario = authenticate(username=nombre_usuario,password=password)
+        usuario = authenticate(username=nombre_usuario, password=password)
 
         if usuario is not None:
             token = RefreshToken.for_user(usuario)
             return Response({
                 "token": str(token.access_token),
+                "usuario": {
+                    "id": usuario.id,
+                    "username": usuario.username,
+                    "email": usuario.email,
+                    "rol": usuario.rol
+                },
                 "mensaje": "Inicio de sesión exitoso"
             })
         else:
             return Response({
                 "error": "Credenciales inválidas"
-            },status=401)
-
+            }, status=401)
 
 
 # Obtener, actualizar o eliminar un usuario específico por su ID
