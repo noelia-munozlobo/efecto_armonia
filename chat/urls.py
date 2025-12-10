31962817f90bee8d58e5e1079fcc288b338dc804
FiorellaCG <fiochaves1303@gmail.com>
2025-12-10 15:17:01 -0600

    cambios

--- chat/urls.py
+++ chat/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import ( ChatCreateView, ConversacionView, MensajesRecibidosView, MensajesEnviadosView, MarcarLeidoView)
+from .views import ( ChatCreateView, ConversacionView, MensajesRecibidosView, 
+                    MensajesEnviadosView, BorrarConversacionView )
 
 urlpatterns = [
     path('chat/', ChatCreateView.as_view(), name='chat-list-create'),
     path('chat/conversacion/<int:remitente_id>/<int:destinatario_id>/', ConversacionView.as_view(), name='conversacion'),
     path('chat/recibidos/<int:destinatario_id>/', MensajesRecibidosView.as_view(), name='mensajes-recibidos'),
     path('chat/enviados/<int:remitente_id>/', MensajesEnviadosView.as_view(), name='mensajes-enviados'),
-    path('chat/marcar-leido/<int:pk>/', MarcarLeidoView.as_view(), name='marcar-leido'),
+    path("chat/borrar-conversacion/<int:remitente_id>/<int:destinatario_id>/", BorrarConversacionView.as_view(), name="borrar_conversacion"),
 ]
--- especialista/tests.py
+++ especialista/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+from usuarios.models import Usuario
+from .models import Especialista
 
-# Create your tests here.
+
+class EspecialistaDeleteTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+        # Crear usuario requerido por el especialista
+        self.usuario = Usuario.objects.create(
+            username="carlos123",
+            first_name="Carlos",
+            last_name="Pérez",
+            email="carlos@example.com",
+            telefono="1111-2222",
+        )
+        self.usuario.set_password("123456")
+        self.usuario.save()
+
+        # Crear el especialista
+        self.especialista = Especialista.objects.create(
+            usuario=self.usuario,
+            especialidad="Psicología Clínica",
+            descripcion="Especialista de pruebas"
+        )
+
+        # URL correcta (según tus rutas reales)
+        self.url = f"/especialistas/especialistas/{self.especialista.id}/"
+
+    def test_eliminar_especialista(self):
+        response = self.client.delete(self.url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        self.assertFalse(
+            Especialista.objects.filter(id=self.especialista.id).exists()
+        )
